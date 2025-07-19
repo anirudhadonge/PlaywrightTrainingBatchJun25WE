86d1897b0b558a213f46700793a7e6f26c1e70c2
Anirudha Donge <anirudha.donge@gmail.com>
2025-07-19 14:35:03 +0530

    Modules

---
+++ Javascript/Session7/Modules.js
@@ -0,0 +1,8 @@
+// modules
+/**
+ * ESModules: Newer version of javascript
+ * Export , Import, Default Export
+ * commonjs: older ECMA 2015 were using commonjs
+ * Modules.Exports, require()
+ * 
+ */
